0654cd9b7f401618ea0d233ed257f56eda6a4ad1
bridder <13068790+bridder@users.noreply.github.com>
2022-11-29 14:51:04 -0500

    Cosmetic changes
    Line breaks, indents, dropped curly braces down a line

--- R/SEIRModel.R
+++ R/SEIRModel.R
@@ -32,23 +32,36 @@
 #' @param method Which integration method to use. Defaults to lsoda
 #' @return a SEIRModel object
 #' @export
-SEIRModel <- function(population, populationFractions, contactMatrix, R0,
-                      latentPeriod, infectiousPeriod, seedInfections, priorImmunity,
-                      useCommunityMitigation, communityMitigationStartDay,
-                      communityMitigationDuration, communityMitigationMultiplier,
-                      simulationLength, seedStartDay, tolerance, method) {
+SEIRModel <- function(population,
+                      populationFractions,
+                      contactMatrix,
+                      R0,
+                      latentPeriod,
+                      infectiousPeriod,
+                      seedInfections,
+                      priorImmunity,
+                      useCommunityMitigation,
+                      communityMitigationStartDay,
+                      communityMitigationDuration,
+                      communityMitigationMultiplier,
+                      simulationLength,
+                      seedStartDay,
+                      tolerance,
+                      method)
+{
   #Check inputs
   specifiedArguments <- names(match.call())[-1]
   argumentList <- lapply(specifiedArguments, as.name)
   names(argumentList) <- specifiedArguments
   parameters <- do.call("checkInputs.SEIR", argumentList) #Get parameters from checked inputs
-
-  initialState <- with(parameters, {
-    c(S  = (1 - priorImmunity) * populationFractions,
-      E  = 0 * populationFractions,
-      I  = 0 * populationFractions,
-      R  = priorImmunity * populationFractions)
-  })
+  
+  initialState <- with(parameters,
+                       {
+                         c(S  = (1 - priorImmunity) * populationFractions,
+                           E  = 0 * populationFractions,
+                           I  = 0 * populationFractions,
+                           R  = priorImmunity * populationFractions)
+                       })
   
   rawOutput <- integrateModel(initialState = initialState,
                               parameters = parameters,
@@ -66,77 +79,106 @@ SEIRModel <- function(population, populationFractions, contactMatrix, R0,
 #' @description Checks the input parameters for the SEIR model
 #' @return List of parameters for the SEIR model
 #' @keywords internal
-checkInputs.SEIR <- function(population = 1, populationFractions = 1, contactMatrix, R0,
-                             latentPeriod, infectiousPeriod, seedInfections = 0, priorImmunity = 0,
-                             useCommunityMitigation = FALSE, communityMitigationStartDay, 
-                             communityMitigationDuration, communityMitigationMultiplier, 
-                             simulationLength = 240, seedStartDay = 0, tolerance = 1e-8, method = "lsoda", ...) {
+checkInputs.SEIR <- function(population = 1,
+                             populationFractions = 1,
+                             contactMatrix,
+                             R0,
+                             latentPeriod,
+                             infectiousPeriod,
+                             seedInfections = 0,
+                             priorImmunity = 0,
+                             useCommunityMitigation = FALSE,
+                             communityMitigationStartDay, 
+                             communityMitigationDuration,
+                             communityMitigationMultiplier, 
+                             simulationLength = 240,
+                             seedStartDay = 0,
+                             tolerance = 1e-8,
+                             method = "lsoda", ...)
+{
   #population
   checkPositiveNumber(population)
   #populationFractions
-  if (!((abs(sum(populationFractions) - 1) < tolerance) && all(populationFractions >= 0))) {
+  if (!((abs(sum(populationFractions) - 1) < tolerance) && all(populationFractions >= 0)))
+  {
     stop("populationFractions must be positive and sum to 1.", call. = FALSE)
   }
   populationFractions <- as.vector(populationFractions) #Drop names to prevent errors in internal data storage
   #contactMatrix
-  if (missing(contactMatrix)) { #If contact matrix is not supplied, default to proportional mixing
+  if (missing(contactMatrix))
+  { #If contact matrix is not supplied, default to proportional mixing
     contactMatrix <- as.matrix(populationFractions)[ , rep(1, length(populationFractions)), drop = FALSE]
-  } else {
-    if (!(all(contactMatrix >=0) && (ncol(contactMatrix) == nrow(contactMatrix))))  {
+  }
+  else
+  {
+    if (!(all(contactMatrix >=0) && (ncol(contactMatrix) == nrow(contactMatrix))))
+    {
       stop("contactMatrix must be square and non-negative.", call. = FALSE)
     }
-    if (length(populationFractions) != ncol(contactMatrix)) {
+    if (length(populationFractions) != ncol(contactMatrix))
+    {
       stop("contactMatrix dimensions do not match populationFractions.", call. = FALSE)
     }
   } 
   #R0
-  if (missing(R0)) {
+  if (missing(R0))
+  {
     stop("R0 must be specified.", call. = FALSE)
   }
   checkPositive(R0)
   #latentPeriod
-  if (missing(latentPeriod)) {
+  if (missing(latentPeriod))
+  {
     stop("latentPeriod must be specified.", call. = FALSE)
   }
   checkPositive(latentPeriod)
   #infectiousPeriod
-  if (missing(infectiousPeriod)) {
+  if (missing(infectiousPeriod))
+  {
     stop("infectiousPeriod must be specified.", call. = FALSE)
   }
   checkPositive(infectiousPeriod)
   #seedInfections
   checkNonNegative(seedInfections)
   checkDimensionsMatch(seedInfections, populationFractions)
-  if (length(seedInfections) == 1) {
-    if (seedInfections > population) {
+  if (length(seedInfections) == 1)
+  {
+    if (seedInfections > population)
+    {
       stop("seedInfections can not exceed the population.", call. = FALSE)
     }
     seedInfections <- seedInfections * populationFractions #Distribute seed infections among population groups proportionately
-  } else if (!all(seedInfections <= (population * populationFractions))) {
+  } else if (!all(seedInfections <= (population * populationFractions)))
+  {
     stop("seedInfections can not exceed the population by group.", call. = FALSE)
   }
   #priorImmunity
   checkBetween0and1(priorImmunity)
   checkDimensionsMatch(priorImmunity, populationFractions)
   #Community Mitigation
-  if (useCommunityMitigation) {
-    if (missing(communityMitigationStartDay)) {
+  if (useCommunityMitigation) 
+  {
+    if (missing(communityMitigationStartDay))
+    {
       stop("communityMitigationStartDay must be specified when using community mitigation.", 
-        call. = FALSE)
+           call. = FALSE)
     }
     checkNonNegative(communityMitigationStartDay)
-    if (missing(communityMitigationDuration)) {
+    if (missing(communityMitigationDuration))
+    {
       stop("communityMitigationDuration must be specified when using community mitigation.", 
-        call. = FALSE)
+           call. = FALSE)
     }
     checkNonNegative(communityMitigationDuration)
-    if (missing(communityMitigationMultiplier)) {
+    if (missing(communityMitigationMultiplier))
+    {
       stop("communityMitigationMultiplier must be specified when using community mitigation.", 
-        call. = FALSE)
+           call. = FALSE)
     }
-    if (!all(dim(communityMitigationMultiplier) == dim(contactMatrix))) {
+    if (!all(dim(communityMitigationMultiplier) == dim(contactMatrix)))
+    {
       stop("Dimensions of communityMitigationMultiplier do not match those of contactMatrix", 
-        call. = FALSE)
+           call. = FALSE)
     }
     checkNonNegative(communityMitigationMultiplier)
   }
@@ -155,7 +197,9 @@ checkInputs.SEIR <- function(population = 1, populationFractions = 1, contactMat
                      seedStartDay = seedStartDay,
                      tolerance = tolerance,
                      method = method)
-  if (useCommunityMitigation) {
+  
+  if (useCommunityMitigation)
+  {
     parameters <- append(parameters,
                          list(communityMitigationStartDay = communityMitigationStartDay, 
                               communityMitigationEndDay = communityMitigationStartDay + communityMitigationDuration,
@@ -165,7 +209,8 @@ checkInputs.SEIR <- function(population = 1, populationFractions = 1, contactMat
 }
 
 #This is a utility function that reconstructs the model state as a list so that equations can refer to compartments by name
-reconstructState.SEIR <- function(state) {
+reconstructState.SEIR <- function(state)
+{
   numberOfClasses <- length(state)/4 #Each of the 4 classes are vectors of the same length
   S  <- state[                       1 :     numberOfClasses ]
   E  <- state[    (numberOfClasses + 1):(2 * numberOfClasses)]
@@ -177,36 +222,43 @@ reconstructState.SEIR <- function(state) {
 #This function implements the multivariate derivative of the SEIR model
 #parameters should define populationFractions, normalizedContactMatrix, beta, lambda, and gamma
 #Note that the total population is normalized to be 1
-getDerivative.SEIR <- function(t, state, parameters) {
+getDerivative.SEIR <- function(t,
+                               state,
+                               parameters)
+{
   stateList <- reconstructState.SEIR(state)
-  with(append(stateList, parameters), {
-    if (useCommunityMitigation) {
-      if ((t >= communityMitigationStartDay) && (t < communityMitigationEndDay)) {
-        contactMatrix <- communityMitigationMultiplier * contactMatrix
-      } 
-    }
-    #Flows
-    forceOfInfection <- beta / populationFractions * (contactMatrix %*% I)
-    
-    S_to_E <- S * forceOfInfection
-    E_to_I <- lambda * E
-    I_to_R <- gamma * I
-    
-    #Derivatives
-    dS <- -S_to_E
-    dE <- S_to_E - E_to_I
-    dI <- E_to_I - I_to_R
-    dR <- I_to_R
-    
-    #Return derivative
-    return(list(c(dS, dE, dI, dR)))
-  })
+  with(append(stateList, parameters),
+       {
+         if (useCommunityMitigation)
+         {
+           if ((t >= communityMitigationStartDay) && (t < communityMitigationEndDay))
+           {
+             contactMatrix <- communityMitigationMultiplier * contactMatrix
+           } 
+         }
+         #Flows
+         forceOfInfection <- beta / populationFractions * (contactMatrix %*% I)
+         
+         S_to_E <- S * forceOfInfection
+         E_to_I <- lambda * E
+         I_to_R <- gamma * I
+         
+         #Derivatives
+         dS <- -S_to_E
+         dE <- S_to_E - E_to_I
+         dI <- E_to_I - I_to_R
+         dR <- I_to_R
+         
+         #Return derivative
+         return(list(c(dS, dE, dI, dR)))
+       })
 }
 
 #This function implements seeding infections in the SEIR model
 #parameters should define seedInfections, lambda, and gamma
 #Note that the total population is normalized to be 1
-doSeed.SEIR <- function(state, parameters) {
+doSeed.SEIR <- function(state, parameters)
+{
   stateList <- reconstructState.SEIR(state)
   with(append(stateList, parameters), {
     seedInfectionsFractions <- seedInfections / population
