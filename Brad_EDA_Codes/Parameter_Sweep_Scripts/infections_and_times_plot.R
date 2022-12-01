bdeb218b41de38e43a2ece9d088ff5938bbb84e6
bridder <13068790+bridder@users.noreply.github.com>
2022-12-01 15:43:49 -0500

    Added tile-dot plot for infections and times for SEIR
    Not really a tile plot, but uses dots to achieve the same effect

--- Brad_EDA_Codes/Parameter_Sweep_Scripts/infections_and_times_plot.R
+++ Brad_EDA_Codes/Parameter_Sweep_Scripts/infections_and_times_plot.R
@@ -45,23 +45,21 @@ p1 <- ggplot(data = x,aes(x = latentPeriod,y = infectiousPeriod,color = peak_inf
 p1 <- p1 + theme(legend.position = "right")
 p1 <- p1 + xlab("tL, Latent Period [days]")
 p1 <- p1 + ylab("tI, Infectious Period [days]")
-#p1 <- p1 + theme(legend.position = "none")
-p1 <- p1 + ggtitle("Peak Infection Value (fractional) as a Function of ti, tL, and R0")
+p1 <- p1 + ggtitle("Peak Infection Value as a Function of ti, tL, and R0")
 
-p1 <-p1 + scale_color_gradientn(colours = c("green","orange","red"), 
-                                values = rescale(c(0,90,365)),
-                                guide = "colorbar", limits=c(0,365))
+p1 <-p1 + scale_color_gradientn(colours = rev(c("magenta","red","orange","yellow","green","blue")), 
+                                values = rescale(c(0,0.2,0.4,0.6,0.8,1.0)),
+                                guide = "colorbar", limits=c(0,1),name= "Fraction")
 
 
-p1 <- p1 + scale_color_gradient2(midpoint=0.5, low="blue", mid="yellow",
-                      high="red", space ="Lab",name = "I_peak" )
 p1 <- p1 + scale_x_continuous(limits = c(0,14),breaks = seq(from = 0,by = 2,to = 14))
 p1 <- p1 + scale_y_continuous(limits = c(0,14),breaks = seq(from = 0,by = 2,to = 14))
 p1 <- p1 + theme(axis.title.x = element_text(face = "bold"))
 p1 <- p1 + theme(axis.title.y = element_text(face = "bold"))
 p1 <- p1 + theme(title = element_text(face = "bold"))
-
+p1 <- p1 + geom_abline(intercept = 0, slope = 1, size = 0.5,color="black",alpha=0.4,linetype="solid")
 p1 <- p1 + theme(aspect.ratio = 1.0)
+p1 <- p1 + theme(legend.position = "top")
 ggsave(filename = "images/SEIR_dot_tile_plot_12_1_2022_sourcing_November_30_2022_data/peak_infection_tiledot_plot.png",device = "png",width = 7.5,height = 7.5,dpi = 300)
 print(p1)
 
@@ -76,8 +74,8 @@ p1 <- p1 + ggtitle("Time-to-Peak Infection as a Function of ti, tL, and R0")
 # p1 <- p1 + scale_color_gradient2(midpoint=0.5, low="red", mid="blue",
 #                                  high="green", space ="Lab" )
 
-p1 <-p1 + scale_color_gradientn(colours = c("red","orange","yellow","green","blue"), 
-                             values = rescale(c(0,30,60,120,180)),
+p1 <-p1 + scale_color_gradientn(colours = c("magenta","red","yellow","orange","green","cyan","blue"), 
+                             values = rescale(c(0,14,30,60,90,180,365)),
                              guide = "colorbar", limits=c(0,365),name= "Days")
 
 p1 <- p1 + scale_x_continuous(limits = c(0,14),breaks = seq(from = 0,by = 2,to = 14))
@@ -87,5 +85,6 @@ p1 <- p1 + theme(axis.title.y = element_text(face = "bold"))
 p1 <- p1 + theme(title = element_text(face = "bold"))
 p1 <- p1 + theme(legend.position = "top")
 p1 <- p1 + theme(aspect.ratio = 1.0)
+p1 <- p1 + geom_abline(intercept = 0, slope = 1, size = 0.5,color="black",alpha=0.4,linetype="solid")
 ggsave(filename = "images/SEIR_dot_tile_plot_12_1_2022_sourcing_November_30_2022_data/day_of_occurrence_of_peak_infection_tiledot_plot.png",device = "png",width = 7.5,height = 7.5,dpi = 300)
 print(p1)
