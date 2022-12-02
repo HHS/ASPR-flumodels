99f63ce1a4c1d830714e676189834a329858a17f
bridder <13068790+bridder@users.noreply.github.com>
2022-12-02 13:01:25 -0500

    Refactored function to be more generalized
    Removed exact state names from within the function

--- Brad_EDA_Codes/Parameter_Sweep_Scripts/convert_model_raw_output_to_tibble.R
+++ Brad_EDA_Codes/Parameter_Sweep_Scripts/convert_model_raw_output_to_tibble.R
@@ -5,11 +5,10 @@ convert_model_raw_output_to_tibble <- function(x,...)
   
   df <- x %>% as_tibble %>% pull(model) %>% unlist(recursive = FALSE)
   df <- df$rawOutput
-  
+  N <- ncol(df)
   df %<>% as_tibble()
-  df %<>% mutate(across(time:R,.fns = as.double))
-  
-  df %<>% pivot_longer(cols = S:R,names_to = "state_variable",values_to = "value") %>% arrange(state_variable,time)
+  df %<>% mutate(across(.cols = everything(),.fns = as.double))
+  df %<>% pivot_longer(cols = 2:N,names_to = "state_variable",values_to = "value") %>% arrange(state_variable,time)
   df %<>% mutate(state_variable = as.factor(state_variable))
   x$tidy_raw_output <- list(df)
   return(x)
