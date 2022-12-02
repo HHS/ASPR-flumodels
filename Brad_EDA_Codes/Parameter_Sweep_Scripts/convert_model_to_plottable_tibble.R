1313787d94b3988347b3c2a4375fa7f4ce8e9a70
bridder <13068790+bridder@users.noreply.github.com>
2022-12-02 09:19:28 -0500

    Deleted deprecated file
    Renamed to convert_model_raw_output_to_tibble

--- Brad_EDA_Codes/Parameter_Sweep_Scripts/convert_model_to_plottable_tibble.R
+++
@@ -1,9 +0,0 @@
-
-df <- x$model[[1]]$rawOutput
-
-df %<>% as_tibble()
-df %<>% mutate(across(time:R,.fns = as.double))
-
-df %<>% pivot_longer(cols = S:R,names_to = "state_variable",values_to = "value") %>% arrange(state_variable,time)
-df %<>% mutate(state_variable = as.factor(state_variable))
-x$tibble <- list(df)
