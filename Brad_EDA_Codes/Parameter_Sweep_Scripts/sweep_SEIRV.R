0b08b6fbf33cfb592cf2215905b4acbb2fc836e1
bridder <13068790+bridder@users.noreply.github.com>
2022-12-02 18:06:11 -0500

    Changed plotting aesthetics and parameter ranges for SEIRV sweep
    vaccine efficacy delay in general has a weak effect on things

--- Brad_EDA_Codes/Parameter_Sweep_Scripts/sweep_SEIRV.R
+++ Brad_EDA_Codes/Parameter_Sweep_Scripts/sweep_SEIRV.R
@@ -3,15 +3,15 @@ library(magrittr)
 library(purrr)
 library(flumodels)
 
-R0_list <- c(2,5,10)
+R0_list <- c(5,10,25)
 
 model_list <- list()
 qq <- 0L
 latentPeriod_list <- c(7,14)
 infectiousPeriod_list <- c(7,14)
-VEs_list <- c(0.1,0.5,0.9)
-VEi_list <- c(0.1,0.5,0.9)
-VEp_list <- c(0.1,0.5,0.9)
+VEs_list <- c(0.3,0.9)
+VEi_list <- c(0.3,0.9)
+VEp_list <- c(0.3,0.9)
 
 par_tibble_list <- list()
 vaccineEfficacyDelay_list <- c(7,14,30) #1 week, 2 weeks, 1 month)
