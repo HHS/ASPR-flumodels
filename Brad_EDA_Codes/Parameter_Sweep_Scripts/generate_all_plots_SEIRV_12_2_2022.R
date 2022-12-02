f324c0f92827152fb1f02d355f542da9b1705065
bridder <13068790+bridder@users.noreply.github.com>
2022-12-02 18:07:13 -0500

    Added sourcing and library() calls to SEIRV big plot function
    Also fixed the output directory

--- Brad_EDA_Codes/Parameter_Sweep_Scripts/generate_all_plots_SEIRV_12_2_2022.R
+++ Brad_EDA_Codes/Parameter_Sweep_Scripts/generate_all_plots_SEIRV_12_2_2022.R
@@ -1,9 +1,10 @@
 invisible(x <- readRDS("sweeps/sweep_SEIRV_RDS_November_30_2022/model_data.rds"))
-  
-source("find_peak_infections.R")
 source("convert_model_raw_output_to_tibble.R")
-source("find_time_of_peak_infections.R")
-source("plot_SEIR.R")
+source("plot_SEIRV.R")
+library(tidyverse)
+library(magrittr)
+library(purrr)
+library(flumodels)
 
 x %<>% group_by(index)
 
@@ -12,11 +13,10 @@ k1 <- list()
 qq <- 0L
 for(smallx in x)
 {
+  print(qq)
   qq <- qq + 1L
   smallx %<>% convert_model_raw_output_to_tibble() %>% bind_rows()
-  smallx %<>% find_peak_infections()
-  smallx %<>% find_time_of_peak_infections()
-  k1[[qq]] <- plot_SEIR(smallx)
-  ggsave(filename = sprintf("images/images_sweep_SEIR_RDS_November_30_2022/%d.png",qq),width = 10,height = 7.5,device = "png",dpi = 300)
+  k1[[qq]] <- plot_SEIRV(smallx)
+  ggsave(filename = sprintf("images/images_sweep_SEIRV_RDS_December_2_2022/%d.png",qq),width = 10,height = 7.5,device = "png",dpi = 300)
 }
 x <- k1 %>% bind_rows
